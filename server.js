e3266afdcad14793afab67c89539f9355c280c49
Glitch (hello-express) <none>
2022-09-25 02:36:13 +0000

    🌪🏬 Checkpoint ./server.js:49102645/1582 ./package.json:49102645/1399

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 // server.js
 const express = require("express");
-const moment = require ('moment').format('DD/MM/YYYY');
+const moment = require("moment");
 
-const app = express()
+moment.tz.setDefault("America/Buenos_Aires");
+
+const app = express();
 const path = require("path");
 const VIEWS = path.join(__dirname, "views");
-app.get("/", (request, response) => { response.send("hola estoy conectado a una api con get: "+moment().format("DD/M/YYYY").toString()) })
+app.get("/", (request, response) => {
+  response.send(
+    "hola estoy conectado a una api con get: " +
+      moment().format("hh:mm").toString()
+  );
+});
 
 app.get("/aboutme", (request, response) => {
-   const datos = 23
-    const mostrar = `<h1>Hola bienvenido a mi primer servidor${datos}<h1>`;
-	response.send(mostrar);
+  const datos = 23;
+  const mostrar = `<h1>Hola bienvenido a mi primer servidor${datos}<h1>`;
+  response.send(mostrar);
 });
 
 app.get("/index", (request, response) => {
-    response.sendFile("index.html", { root : VIEWS }
-    );
+  response.sendFile("index.html", { root: VIEWS });
 });
 
 const port = 8080;
 app.listen(port, () => {
-	console.log(`server listening on port ${port}`);
+  console.log(`server listening on port ${port}`);
 });
-
-
