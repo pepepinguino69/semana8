a61739ac16c481493de0ce3ce065947bcbe25604
Glitch (hello-express) <none>
2022-09-25 03:24:08 +0000

    🌹💅 Checkpoint ./server.js:49102645/374

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const luxon= require ("luxon")
 const { DateTime } = require("luxon");
 
 
-import { format, formatDistance, formatRelative, subDays } from 'date-fns'
-let ahora = DateTime.now().setZone('America/Argentina/Buenos_Aires').minus({weeks:1}).endOf('day').toISO();
-
+let datos = DateTime.now().setZone('America/Argentina/Buenos_Aires').toLocaleString({ month: 'long', day: 'numeric',year:'numeric',hour:'numeric',minute:'numeric' })
 const app = express();
 const path = require("path");
 const VIEWS = path.join(__dirname, "views");
@@ -19,9 +17,8 @@ app.get("/", (request, response) => {
 
 app.get("/aboutme", (request, response) => {
   
- const datos =32
 
-  const mostrar = `<h1>Hola bienvenido a mi primer servidor${datos}<h1>`;
+  const mostrar = `<h1>Hola bienvenido a mi primer servidor ${datos}<h1>`;
   response.send(mostrar);
 });
 
