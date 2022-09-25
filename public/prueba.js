b004a61b6793cdf79e178866567712baf97258e1
Glitch (hello-express) <none>
2022-09-25 19:01:42 +0000

    💏🌜 Checkpoint ./public/prueba.js:49102645/167

--- public/prueba.js
+++ public/prueba.js
@@ -1,6 +1,7 @@
 
           const hora=document.querySelector("h1");
           const fecha = Date.now()
-          mes =
+          const UTC =parseInt((fecha/365/24/3600/1000))+1970
+          const mes=UTC
 
-          hora.innerHTML+=`${fecha}`
+          hora.innerHTML+=`${mes}`
