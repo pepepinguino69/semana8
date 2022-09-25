deee7107edf2da40539a8fbe6d043ba4d0071e40
Glitch (hello-express) <none>
2022-09-25 01:37:13 +0000

    🚝⛴ Checkpoint ./server.js:49102645/556 ./index.html:49102645/10302 ./views/index.html :49102645/5151

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const path = require("path");
 const VIEWS = path.join(__dirname, "views");
 app.get("/", (request, response) => { response.send("hola estoy conectado a una api con get") })
 
-app.get("/index", (request, response) => {
-    response.sendFile("index.html", { root : VIEWS }
-    );
-});
 app.get("/aboutme", (request, response) => {
-    const datos = 23
+   const datos = 23
     const mostrar = `<h1>Hola bienvenido a mi primer servidor${datos}<h1>`;
 	response.send(mostrar);
 });
 
-const port = 80;
+app.get("/index", (request, response) => {
+    response.sendFile("index.html", { root : VIEWS }
+    );
+});
+
+const port = 8080;
 app.listen(port, () => {
 	console.log(`server listening on port ${port}`);
 });
