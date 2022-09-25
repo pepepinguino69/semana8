11f056c6c820be3c9f740cd34e41bc50d5aaee71
Glitch (hello-express) <none>
2022-09-25 18:50:41 +0000

    🏢👩‍👩‍👦 Checkpoint ./public/prueba.js:49102645/1342

--- public/prueba.js
+++ public/prueba.js
@@ -1,7 +1,6 @@
-          const luxon= import ("luxon")
-          const { DateTime } = require("luxon");
+
           const hora=document.querySelector("h1");
-          let datos = DateTime.now().setZone('America/Argentina/Buenos_Aires').toLocaleString({ month: 'long', day: 'numeric',year:'numeric',hour:'numeric',minute:'numeric' })
-          console.log(datos)
-          hora.innerHTML+=`pepe`
-          alert("y")
+          const fecha = Date.now()
+          mes =
+
+          hora.innerHTML+=`${fecha}`
