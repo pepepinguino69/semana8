6ace9823b121e3126a5fad6e699ba16a9457027c
Glitch (hello-express) <none>
2022-09-26 18:42:40 +0000

    🌼🔨 Checkpoint ./server.js:49102645/5 ./views/addProduct.html:49102645/44

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/index.html', (req, res) => res.sendFile(__dirname + '/views/index.html
 app.get('/index', (req, res) => res.sendFile(__dirname + '/views/index.html'))
 app.get('/signup.html', (req, res) => res.sendFile(__dirname + '/views/signup.html'))
 app.get('/addProduct.html', (req, res) => res.sendFile(__dirname + '/views/addProduct.html'))
-app.get('/addProduct.html', (req, res) => res.sendFile(__dirname + '/views/addProduct.html'))
+app.get('/addProduct', (req, res) => res.sendFile(__dirname + '/views/addProduct.html'))
 app.get('/signup', (req, res) => res.sendFile(__dirname + '/views/signup.html'))
 app.get('/json/producto/:id',(req, res) => {const { id } = req.params;myInstance.getById(id).then((data) => res.json(data))});
 app.get('/json/productoRandom/', (req, res) => {myInstance.getById(-1).then((data) => res.json(data))});
