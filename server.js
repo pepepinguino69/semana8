82a7dc7614dcaaae9dc4166c297b650230fee80e
Glitch (hello-express) <none>
2022-09-26 06:42:16 +0000

    🏜🏋 Checkpoint ./public/card.css:49102645/27 ./server.js:49102645/1186

--- server.js
+++ server.js
@@ -29,21 +29,19 @@ const app = express();
 const path = require("path");
 const VIEWS = path.join(__dirname, "views");
 app.set('view engine', 'pug')
-app.get('/productoRandom',async (req, res) => await myInstance.getById(-1).then((data)=>res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url,nombre:'Ariel Rubel'})))
-app.get('/productos/:id', async (req, res) => {const { id } = req.params;await myInstance.getById(id).then((data)=>res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url,nombre:'Ariel Rubel'}))})
 
 
 let acum=""
+app.get("/", (req, res) => res.sendFile(__dirname + '/views/home.html'))
 app.get("/productos", (req, res) => {myInstance.getAll().then((data)=>{data.forEach(e=>acum+=`<img src="${e.url}"><h3>${e.id}-${e.title}----${e.price}---</h3>`);res.send(acum)})});
-
-app.get('/json/productos',(req, res) => {myInstance.getAll().then((data) => res.json(data))});
+app.get("/productoRandom",async (req, res) => await myInstance.getById(-1).then((data)=>res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url,nombre:'Ariel Rubel'})))
+app.get("/productos/:id", async (req, res) => {const { id } = req.params;await myInstance.getById(id).then((data)=>res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url,nombre:'Ariel Rubel'}))})
+app.get("/json/productos",(req, res) => {myInstance.getAll().then((data) => res.json(data))});
 app.get("/index", (req, res) => res.sendFile(__dirname + '/views/index.html'))
 app.get("/signup", (req, res) => res.sendFile(__dirname + '/views/signup.html'))
-//app.get('/productoRandom', (req, res) =>myInstance.getById(-1).then((data) => res.send(`<h3>${data.id}</h3><h3>${data.title}</h3><h3>${data.price}</h3><img src="${data.url}">`)));
-app.get('/json/productos/:id', (req, res) => {const { id } = req.params;myInstance.getById(id).then((data) => res.json(data))});
-app.get('/json/productoRandom/', (req, res) => {myInstance.getById(-1).then((data) => res.json(data))});
+app.get("/json/productos/:id",(req, res) => {const { id } = req.params;myInstance.getById(id).then((data) => res.json(data))});
+app.get("/json/productoRandom/", (req, res) => {myInstance.getById(-1).then((data) => res.json(data))});
 app.use(express.static('public'));
-app.get("/", (req, res) => res.sendFile(__dirname + '/views/home.html'))
 
 
 app.get("/index", (request, response) => {
