7e55c6be2341b63b527fa55c7f8924c7ed784c37
Glitch (hello-express) <none>
2022-09-27 10:27:36 +0000

    🚟☂️ Checkpoint ./views/card3.pug:49102645/3 ./public/modify.js:49102645/475

--- public/modify.js
+++ public/modify.js
@@ -1,4 +1,3 @@
 const btnModify =document.querySelector('#btn1')
-btnModify.addEventListner('click',alert('modificar'))
-const btnDelete =document.querySelector('#btn2')
-btnDelete.addEventListner('click',e=>alert('borrar'))
+btnModify.addEventListener("click", (e) => {alert('a')})
+
