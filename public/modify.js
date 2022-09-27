daf47d295ba498d4d9bda9fe8a6dcd5d257bec30
Glitch (hello-express) <none>
2022-09-27 10:15:30 +0000

    🚄🎰 Checkpoint ./public/modify.js:49102645/1261

---
+++ public/modify.js
@@ -0,0 +1,4 @@
+const btnModify =document.querySelector('#btn1')
+btnModify.addEventListner('click',alert('modificar'))
+const btnDelete =document.querySelector('#btn2')
+btnDelete.addEventListner('click',e=>alert('borrar'))
