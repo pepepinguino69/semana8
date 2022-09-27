66b6d9c32aa8d5a82e504fb5e6bf3054287fac22
Glitch (hello-express) <none>
2022-09-27 11:15:24 +0000

    🚖🎐 Checkpoint ./public/modify.js:49102645/129

--- public/modify.js
+++ public/modify.js
@@ -1,7 +1,8 @@
 const btnModify =document.querySelector('#btn1')
-const btnModify =document.querySelector('#btn1')
+const btnDelete=document.querySelector('#btn2')
 const record =document.querySelector('#id').textContent
 
 
 btnModify.addEventListener("click", (e) => {alert('modify '+record)})
+btnModify.addEventListener("click", (e) => {myInstance.deleteById(record)})
 
