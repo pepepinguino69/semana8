131dafce03f9c6a5cf0fa1867b5914406e4bfc2b
Glitch (hello-express) <none>
2022-10-04 12:14:16 +0000

    🌠🌉 Checkpoint ./routes/routes.js:49102645/3548 ./server.js:49102645/2

--- routes/routes.js
+++ routes/routes.js
@@ -14,14 +14,14 @@ const productsRouter = express.Router();
 
 const myInstance = new Contenedor("productos.txt");
 
+productsRouter.get("/",(req,res)=>{res.send("peticion recibida desde router produyctos")})
+productsRouter.delete('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
+productsRouter.put('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
 productsRouter.get('/api/productos/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
+productsRouter.post('/api/productos/', function(req, res){myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
 
 
-
-
-
-productsRouter.get("/home",(req,res)=>{
-    res.send("peticion home")
-})
+//productRouter.get("/home",(req,res)=>{
+//    res.send("peticion home")})
 
 module.exports = productsRouter;
--- server.js
+++ server.js
@@ -11,14 +11,14 @@
 
 
 const express = require("express");
-const productRouter = require("./routes/routes");
+const productsRouter = require("./routes/routes");
 
 const app = express();
 
 app.listen(8080,()=>console.log("server listening on port 8080"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use("/api/productos", productRouter);
+app.use("/api/productos", productsRouter);
 
 
 
