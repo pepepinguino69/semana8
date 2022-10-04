f15785f4eb5617557a6a8c264e4a269003247a3d
Glitch (hello-express) <none>
2022-10-04 06:38:53 +0000

    🌎🎱 Checkpoint ./server.js:49102645/401 ./routes/routes.js:49102645/301

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
-// server.js
-const express = require("express");
-const productRouter = require("./routes/routes")
-const app = express();
-const port = 8080;
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-});
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use("/api/productos", productRouter);
-const path = require("path");
-app.use(express.static('public'))
 
 const express = require("express");
-const productRouter = require("./routes/products");
+const productRouter = require("./routes/routes");
 
 const app = express();
 
