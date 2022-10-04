55e77c8ce07843001e9f09b1a9eb2e38d97bfa6c
Glitch (hello-express) <none>
2022-10-04 06:27:01 +0000

    🍔⏲ Checkpoint ./server.js:49102645/1390 ./routes/routes.js:49102645/20

--- server.js
+++ server.js
@@ -12,18 +12,17 @@ app.use(express.urlencoded({extended: true}))
 app.use("/api/productos", productRouter);
 const path = require("path");
 app.use(express.static('public'))
-let acum=""
-//app.get("/", (req, res) => res.sendFile(__dirname + '/views/home.html'))
 
+const express = require("express");
+const productRouter = require("./routes/products");
 
-//app.delete('/json/producto/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
-//app.put('/json/producto/:xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
-//app.get('/json/producto/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
-//app.get('/crud.html', (req, res) => res.sendFile(__dirname + '/views/crud.html'))
-//app.post('/addProduct/', function(req, res){
-//myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
-//app.get('/signup', (req, res) => res.sendFile(__dirname + '/views/signup.html'))
-//app.get('/json/productos', (req, res) => myInstance.getAll().then((data) => res.json({data})))
+const app = express();
 
-  
+app.listen(8080,()=>console.log("server listening on port 8080"));
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api/productos", productRouter);
+
+// posibles rutas
