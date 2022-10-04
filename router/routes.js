cb5b4f3d2e6c9e2b2d5ce1050e60e57e4d865728
Glitch (hello-express) <none>
2022-10-04 02:44:00 +0000

    😇🦂 Checkpoint ./router/routes.js:49102645/456

--- router/routes.js
+++ router/routes.js
@@ -9,8 +9,6 @@ productRouter.get("/",(req,res)=>res.send("peticion recibida desde router produy
 
 
 
-module.exports = productRouter()
-
 
 
 app.use(express.json())
@@ -23,21 +21,15 @@ app.use(express.static('public'))
 const myInstance = new Contenedor("productos.txt");
 app.use(express.static('public'));
 let acum=""
-//app.get("/", (req, res) => res.sendFile(__dirname + '/views/home.html'))
 
-app.get('/productoRandom', (req, res) => {
-        myInstance.getById(-1).then((data)=>
-              res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url,nombre:'Ariel Rubel'}))})
-
-
-app.delete('/json/producto/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
-app.put('/json/producto/:xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
-app.get('/json/producto/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
+producttRouter.delete('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
+productRouter.put('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
+app.get('/api/productos/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
 app.get('/crud.html', (req, res) => res.sendFile(__dirname + '/views/crud.html'))
 app.post('/addProduct/', function(req, res){
 myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
 //app.get('/signup', (req, res) => res.sendFile(__dirname + '/views/signup.html'))
-app.get('/json/productos', (req, res) => myInstance.getAll().then((data) => res.json({data})))
+app.get('/api/productos', (req, res) => myInstance.getAll().then((data) => res.json({data})))
 
 
 app.get('/productos', (req, res) => {
@@ -50,3 +42,5 @@ const port = 8080;
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
+
+module.exports = productRouter()
