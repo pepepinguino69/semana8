a88ecf95a0cace9dd5738f26387d39d0424a3ff7
Glitch (hello-express) <none>
2022-10-04 13:13:22 +0000

    🎠🚖 Checkpoint ./routes/routes.js:49102645/10

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,7 @@ const myInstance = new Contenedor("productos.txt");
 productsRouter.get("/",(req,res)=>{res.send("peticion recibida desde router productos")})
 productsRouter.delete('/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
 productsRouter.put(':xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
-productsRouter.get('/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(parseInt(xx)).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
+productsRouter.get('/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
 productsRouter.post('/', function(req, res){myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
 //productsRouter.get("home",(req,res)=>{res.send("peticion home")})
 
