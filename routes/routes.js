d4069ab2a538518d887a6cffb8bd9dd09c8577f5
Glitch (hello-express) <none>
2022-10-04 14:12:22 +0000

    🗼🍫 Checkpoint ./views/crud.html:49102645/181 ./routes/routes.js:49102645/61

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,7 @@ const myInstance = new Contenedor("productos.txt");
 productsRouter.get("/",(req,res)=>myInstance.getAll().then((data) => res.json(data)))
 productsRouter.delete('/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
 productsRouter.put(':xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
-productsRouter.get('/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
+productsRouter.get('/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({data}))})
 productsRouter.post('/', function(req, res){myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
 
 
