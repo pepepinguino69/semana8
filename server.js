6ec0e521b17c23468b0a9f5d479b3fd9c4e9d984
Glitch (hello-express) <none>
2022-10-04 07:26:01 +0000

    💄💒 Checkpoint ./routes/routes.js:49102645/90 ./server.js:49102645/3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const productRouter = require("routes/routes");
+const productRouter = require("/routes/routes");
 const app = express();
 app.listen(8080,()=>console.log("server listening on port 8080"));
 app.use(express.json());
