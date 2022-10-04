36d42716482252655de3e5c3df0a547837929777
Glitch (hello-express) <none>
2022-10-04 07:01:56 +0000

    🎸🏤 Checkpoint ./routes/routes.js:49102645/38 ./server.js:49102645/13

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,20 @@
 // server.js
 const express = require("express");
-const  Contenedor  = require('../Contenedor.js').Contenedor
-//const productsRouter = express.Router();
+const  Contenedor  = require('Contenedor.js').Contenedor
+const productRouter = express.Router();
 
 const myInstance = new Contenedor("productos.txt");
 
 
-productsRouter.get("/",(req,res)=>{res.send("peticion recibida desde router produyctos")})
-//productsRouter.delete('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
-//productsRouter.put('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
-//productsRouter.get('/api/productos/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
-//productsRouter.post('/api/productos/', function(req, res){myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
+productRouter.get("/",(req,res)=>{res.send("peticion recibida desde router produyctos")})
+productRouter.delete('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.deleteById(xx).then((data) => res.send(data))});
+productRouter.put('/api/productos/:xx', (req, res) => {const { xx } = req.params;myInstance.putById(xx,req.body).then ((data)=> res.send('modificado'))})
+productRouter.get('/api/productos/:xx',  (req, res) => {const { xx } = req.params;myInstance.getById(xx).then((data) => res.json({id:data.id,price:data.price,title:data.title,url:data.url}))})
+productRouter.post('/api/productos/', function(req, res){myInstance.save(req.body)    .then((data) => myInstance.getById(req.body.id).then((data) => res.render('card',{id:data.id,price:data.price,title:data.title,img:data.url})))});
 
 const port = 8080;
-app.listen(port, () => {
+productRouter.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
 
-module.exports = productsRouter()
+module.exports = productRouter()
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
 const express = require("express");
-const productRouter = require("./routes/routes");
+//const productRouter = require("/routes/routes");
 const app = express();
 app.listen(8080,()=>console.log("server listening on port 8080"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use("/api/productos", productRouter);
+//app.use("/api/productos", productRouter);
 
 // posibles rutas
