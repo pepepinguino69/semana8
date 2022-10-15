da75d6933a5332ccbae494acd42317b660c4e22b
Glitch (hello-express) <none>
2022-10-15 16:20:37 +0000

    🌞🌄 Checkpoint ./server.js:49102645/594

--- server.js
+++ server.js
@@ -6,19 +6,8 @@ const app = express();
 
 app.listen(8080,()=>console.log("server listening on port 8080"));
 
-
-app.use(express.static(_dirname))
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 const path = require("path");
 const VIEWS = path.join(__dirname, "views");
-app.set('view engine', 'pug')
-app.use(express.static('public'));
-app.use("/api/productos", productsRouter);
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/crud.html'))
-app.get('/signup', (req, res) => res.sendFile(__dirname + '/views/signup.html'))
-app.get('/index', (req, res) => res.sendFile(__dirname + '/views/index.html'))
-app.get('/handlebar1', (req, res) => res.sendFile(__dirname + '/views/handlebar1.html'))
-
-
-
+app.use(express.static(__dirname,'public'))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
