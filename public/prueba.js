32e12a087ecf76ad30f236372519528ecc5e2fca
Glitch (hello-express) <none>
2022-10-18 13:36:34 +0000

    ⛱☺️ Checkpoint ./server.js:49102645/20

--- public/prueba.js
+++
@@ -1,7 +0,0 @@
-
-          const hora=document.querySelector("#time");
-          const fecha = new Date
-          const ano = fecha.getFullYear()
-          const mes = fecha.getMonth()+1
-          const dia = fecha.getDate()
-          hora.innerHTML+=` al:  ${dia}-${mes}- ${ano}`
